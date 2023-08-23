1687b4ce46468517b4e85f16d12c6be9812169cb
jmmjml <116905479+jmmjml@users.noreply.github.com>
2023-08-23 13:36:47 +0000

    guardando 1

--- App.js
+++ App.js
@@ -10,10 +10,43 @@ import {
 import styles from './globalStyles';
 
 export default function App() {
+  const [valorLitro, setValorLitro] = useState('');
+  const [quantidade, setQuantidade] = useState('');
+  const [km, setKm] = useState('');
+  const [resultado, setResultado] = useState('');
+
+  const executar = () =>{
+  }
+
+
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-      {/* <TextInput value={} onChangeText={} style={styles.texto} keyboardType='numeric'></TextInput> */}
+        <View>
+          <Text>Valor do litro</Text>
+          <View style={styles.viewInput}>
+            <TextInput value={valorLitro} onChangeText={setValorLitro} style={styles.texto} keyboardType='numeric'></TextInput>
+          </View>
+        </View>
+        <View>
+          <Text>Quantidade Abastecida</Text>
+          <View style={styles.viewInput}>
+            <TextInput value={quantidade} onChangeText={setQuantidade} style={styles.texto} keyboardType='numeric'></TextInput>
+          </View>
+        </View>
+        <View>
+          <Text>Km Rodado</Text>
+          <View style={styles.viewInput}>
+            <TextInput value={km} onChangeText={setKm} style={styles.texto} keyboardType='numeric'></TextInput>
+          </View>
+        </View>
+        <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+          <Pressable style={styles.botao} ><Text style={{color:"#fff"}}>Buscar valor</Text></Pressable>
+        </View>
+        <View>
+          <Text>{`Resultado: ${resultado}`}</Text>
+        </View>
       <StatusBar style="auto" />
       </View>
     </View>
